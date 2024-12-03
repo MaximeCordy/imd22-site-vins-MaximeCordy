84e23940ba9fba9dbf5001c78943880966139beb
MaximeCordy <maxime.cordy@studentfr.ch>
2024-12-03 09:11:29 +0100

    nouveau bouton

--- dist/index.18dbc454.js
+++ dist/index.18dbc454.js
@@ -584,6 +584,20 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"1SICI":[function(require,module,exports) {
+// erase the input
+$("#erase").click(function() {
+    $(this).prev().val("");
+});
+// preview and hide password
+$("#showPassword").click(function() {
+    var foo = $(this).prev().attr("type");
+    if (foo == "password") $(this).prev().attr("type", "text");
+    else $(this).prev().attr("type", "password");
+});
+//show help
+$("#showHelp").click(function() {
+    $("#help").toggle();
+});
 
 },{}]},["j2YDk","1SICI"], "1SICI", "parcelRequire0300")
 
---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+// erase the input
+$("#erase").click(function () {
+  $(this).prev().val("");
+});
+
+// preview and hide password
+$("#showPassword").click(function () {
+  var foo = $(this).prev().attr("type");
+  if (foo == "password") {
+    $(this).prev().attr("type", "text");
+  } else {
+    $(this).prev().attr("type", "password");
+  }
+});
+
+//show help
+$("#showHelp").click(function () {
+  $("#help").toggle();
+});
